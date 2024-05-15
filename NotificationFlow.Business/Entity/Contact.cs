960be1d8326239e3f4c155ade7a2b155226c181e
André Nascimento Giuseppin <andre.giuseppin@gmail.com>
2024-05-14 23:06:31 -0300

    Add database entities

---
+++ NotificationFlow.Business/Entity/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace NotificationFlow.Business.Entity
+{
+    public class Contact : Entity
+    {
+        public int Id { get; set; }
+        public int ContactType { get; set; }
+        public string Value { get; set; } = string.Empty;
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ NotificationFlow.Business/Entity/User.cs
@@ -0,0 +1,12 @@
+﻿namespace NotificationFlow.Business.Entity
+{
+    public class User : Entity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Document { get; set; } = string.Empty;
+
+        public List<Contact> Contacts { get; set; }
+        public List<NotificationUser> NotificationUsers { get; set; }
+    }
+}
