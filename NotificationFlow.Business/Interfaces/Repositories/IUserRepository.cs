84df3a7e4804139fad9f66ad84fd24866c055e09
André Nascimento Giuseppin <andre.giuseppin@gmail.com>
2024-05-15 19:09:14 -0300

    add user repository add DI for user

---
+++ NotificationFlow.Business/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using NotificationFlow.Business.Entity;
+
+namespace NotificationFlow.Business.Interfaces.Repositories
+{
+    public interface IUserRepository
+    {
+        Task Post(User user);
+    }
+}
---
+++ NotificationFlow.Business/Mappers/UserMapper.cs
@@ -0,0 +1,31 @@
+﻿using NotificationFlow.Business.Entity;
+using NotificationFlow.Business.Enums;
+using NotificationFlow.Business.Models;
+
+namespace NotificationFlow.Business.Mappers
+{
+    public static class UserMapper
+    {
+        public static User UserRequestToUser(UserRequest userRequest)
+        {
+            return new User
+            {
+                Name = userRequest.Name,
+                Document = userRequest.Document,
+                Contacts = new List<Contact>
+                {
+                    new Contact
+                    {
+                        ContactType = (int)ContactTypeEnum.Email,
+                        Value = userRequest.Email
+                    },
+                    new Contact
+                    {
+                        ContactType = (int)ContactTypeEnum.Phone,
+                        Value = userRequest.Phone
+                    }
+                }
+            };
+        }
+    }
+}
