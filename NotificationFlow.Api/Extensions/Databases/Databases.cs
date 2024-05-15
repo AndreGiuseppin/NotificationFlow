25e7f7735a41de3dc2e05e63d13af445f69089ae
André Nascimento Giuseppin <andre.giuseppin@gmail.com>
2024-05-14 23:23:14 -0300

    add sqlServer configuration add connectionString

--- NotificationFlow.Api/Extensions/Databases/Databases.cs
+++ NotificationFlow.Api/Extensions/Databases/Databases.cs
@@ -1,9 +1,14 @@
-﻿namespace NotificationFlow.Api.Extensions.Databases
+﻿using Microsoft.EntityFrameworkCore;
+using NotificationFlow.Data.Database.SqlServer;
+
+namespace NotificationFlow.Api.Extensions.Databases
 {
     public static class Databases
     {
-        public static IServiceCollection AddSqlServer(this IServiceCollection services)
+        public static IServiceCollection AddSqlServer(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration["Databases:ConnectionString"].ToString()));
 
             return services;
         }
