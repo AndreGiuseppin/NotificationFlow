1f46737940cd5dfd17d77a97e06fe80e118dbf26
André Nascimento Giuseppin <andre.giuseppin@gmail.com>
2024-05-15 19:08:35 -0300

    add user controller

---
+++ NotificationFlow.Api/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotificationFlow.Business.Interfaces.Services;
+using NotificationFlow.Business.Models;
+
+namespace NotificationFlow.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] UserRequest request, [FromServices] IUserService service)
+        {
+            await service.Post(request);
+            return Created();
+        }
+    }
+}
