b025d7babed1dc1b46b71ba23c901dd18b4220fd
André Nascimento Giuseppin <andre.giuseppin@gmail.com>
2024-05-14 22:31:20 -0300

    Add ApplicationDbContext

---
+++ NotificationFlow.Business/Entity/Entity.cs
@@ -0,0 +1,14 @@
+﻿namespace NotificationFlow.Business.Entity
+{
+    public class Entity : ISoftDeleteEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+
+    public interface ISoftDeleteEntity
+    {
+        public DateTime? DeletedAt { get; set; }
+    }
+}
---
+++ NotificationFlow.Data/DbContexts/SqlServer/ApplicationDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotificationFlow.Business.Entity;
+using NotificationFlow.Data.DbContexts.SqlServer.Extensions;
+
+namespace NotificationFlow.Data.Database.SqlServer
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            modelBuilder.ApplyGlobalFilters<ISoftDeleteEntity>(e => e.DeletedAt == null);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(
+            CancellationToken cancellationToken = default)
+        {
+            UpdateAuditDates();
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void UpdateAuditDates()
+        {
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.CurrentValues[nameof(Entity.CreatedAt)] = DateTime.UtcNow;
+                        break;
+                    case EntityState.Modified:
+                        entry.CurrentValues[nameof(Entity.UpdatedAt)] = DateTime.UtcNow;
+                        break;
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Modified;
+                        entry.CurrentValues[nameof(Entity.DeletedAt)] = DateTime.UtcNow;
+                        break;
+                }
+            }
+        }
+    }
+}
