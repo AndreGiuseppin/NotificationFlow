7958d36710a81fbbfeaef2d771576c8d68e5afd6
André Nascimento Giuseppin <andre.giuseppin@gmail.com>
2024-05-14 22:18:38 -0300

    Add Extensions Folder Use extensions folder in program.cs

---
+++ NotificationFlow.Api/Extensions/ApplicationServices/ApplicationServices.cs
@@ -0,0 +1,15 @@
+﻿namespace NotificationFlow.Api.Extensions.ApplicationService
+{
+    public static class ApplicationServices
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
